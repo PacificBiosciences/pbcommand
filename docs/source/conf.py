25353bd477b3a403fedcc3a8bd808459b732913a
mpkocher <michael.kocher@me.com>
2015-12-17 18:42:43 -0800

    Add CLI docs for pbservice using sphinx-argparse.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,9 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+# For argparse
+extensions += ['sphinxarg.ext']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
---
+++ docs/source/services.rst
@@ -0,0 +1,9 @@
+
+SMRT Service commandline interface
+==================================
+
+
+.. argparse::
+   :module: pbcommand.services.cli
+   :func: get_parser
+   :prog: pbservice
