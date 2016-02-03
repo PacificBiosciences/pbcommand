a0c01f60955b3a4cd40443516956bb8ee16e0906
natechols <nechols@pacificbiosciences.com>
2016-02-03 10:26:42 -0800

    moving a couple of functions back to pbsmrtpipe since they are not required anywhere else

--- pbcommand/cli/utils.py
+++ pbcommand/cli/utils.py
@@ -78,21 +78,6 @@ add_subread_input = _add_input_file("subread_ds", validate_file, "Path to PacBio
 add_ds_reference_input = _add_input_file("reference_ds", validate_file, "Path to PacBio Subread DataSet XML")
 
 
-def add_override_distribute_option(p):
-    g = p.add_mutually_exclusive_group()
-    g.add_argument('--force-distributed', action='store_const', const=True, default=None,
-                   help="Override XML settings to enable distributed mode (if cluster manager is provided)")
-    g.add_argument('--local-only', action='store_const', const=True, default=None,
-                   help="Override XML settings to disable distributed mode. All Task will be submitted to {n}".format(n=platform.node()))
-    return p
-
-
-def add_override_chunked_mode(p):
-    g = p.add_mutually_exclusive_group()
-    g.add_argument('--force-chunk-mode', action='store_const', const=True, default=None, help="Override to enable Chunk mode")
-    g.add_argument('--disable-chunk-mode', action='store_const', const=True, default=None, help="Override to disable Chunk mode")
-    return p
-
 def args_executer(args):
     """
 
