740db0239f840cc788dec5ac5169b645f2e0d880
natechols <nathaniel.echols@gmail.com>
2016-08-29 10:06:52 -0700

    accumulate errors as strings

--- pbcommand/validators.py
+++ pbcommand/validators.py
@@ -96,41 +96,38 @@ def fofn_to_files(fofn):
         raise IOError("Unable to find FOFN {f}".format(f=fofn))
 
 
-def validate_report(file_name, raise_failure=True):
+def validate_report(file_name):
     e = []
     base_path = os.path.dirname(file_name)
     r = load_report_from_json(file_name)
     if r.title is None:
-        e.append(ValueError("Report {i} is missing a title".format(i=r.id)))
+        e.append("Report {i} is missing a title".format(i=r.id))
     for t in r.tables:
         if t.title is None:
-            e.append(ValueError("Table {r.t} is missing a title".format(
-                                r=r.id, t=t.id)))
+            e.append("Table {r.t} is missing a title".format(r=r.id, t=t.id))
         for col in t.columns:
             if col.header is None:
-                e.append(ValueError("Column {r.t.c} is missing a header".format(
-                                    r=r.id, t=t.id, c=col.id)))
+                e.append("Column {r.t.c} is missing a header".format(
+                         r=r.id, t=t.id, c=col.id))
         lengths = set([len(col.values) for col in t.columns])
         if len(lengths) != 1:
-            e.append(ValueError(
-                "Inconsistent column sizes in table {r.t}: {s}".format(
-                r=r.id, t=t.id, s=",".join(
-                [str(x) for x in sorted(list(lengths))]))))
+            e.append("Inconsistent column sizes in table {r.t}: {s}".format(
+                     r=r.id, t=t.id, s=",".join(
+                     [str(x) for x in sorted(list(lengths))])))
     for pg in r.plotGroups:
         if pg.title is None:
-            e.append(ValueError("Plot group {r.g} is missing a title".format(
-                                r=r.id, g=pg.id)))
+            e.append("Plot group {r.g} is missing a title".format(
+                     r=r.id, g=pg.id))
         for plot in pg.plots:
             #if plot.caption is None:
             #    raise ValueError("Plot {r.g.p} is missing a caption".format(
             #                     r=r.id, g=pg.id, p=plot.id))
             if plot.image is None:
-                e.append(ValueError("Plot {r.g.p} does not have an image".format(
-                                    r=r.id, g=pg.id, p=plot.id)))
+                e.append("Plot {r.g.p} does not have an image".format(
+                         r=r.id, g=pg.id, p=plot.id))
             img_path = os.path.join(base_path, plot.image)
             if not os.path.exists(img_path):
-                e.append(ValueError("The plot image {f} does not exist".format(
-                                    f=img_path)))
+                e.append("The plot image {f} does not exist".format(f=img_path))
             if plot.thumbnail is None:
                 pass
                 #raise ValueError("Plot {r.g.p} does not have an thumbnail image".format(
@@ -138,11 +135,11 @@ def validate_report(file_name, raise_failure=True):
             else:
                 thumbnail = os.path.join(base_path, plot.thumbnail)
                 if not os.path.exists(thumbnail):
-                    e.append(ValueError("The thumbnail image {f} does not exist".format(f=img_path)))
+                    e.append("The thumbnail image {f} does not exist".format(f=img_path))
         if pg.thumbnail is not None:
             thumbnail = os.path.join(base_path, pg.thumbnail)
             if not os.path.exists(thumbnail):
-                e.append(ValueError("The thumbnail image {f} does not exist".format(f=img_path)))
-    if raise_failure and len(e) > 0:
-        raise e[0]
-    return e
+                e.append("The thumbnail image {f} does not exist".format(f=img_path))
+    if len(e) > 0:
+        raise ValueError("\n".join(e))
+    return r
