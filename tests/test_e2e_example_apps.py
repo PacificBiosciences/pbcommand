046bc5b89f82e3c16e59eee20fe9ccb0892182a6
mpkocher <michael.kocher@me.com>
2016-09-06 16:35:19 -0700

    Use PbTestApp in tests.

--- tests/test_e2e_example_apps.py
+++ tests/test_e2e_example_apps.py
@@ -1,19 +1,19 @@
 import logging
 
 from base_utils import get_data_file
-import pbcommand.testkit
+from pbcommand.testkit import PbTestApp
 
 log = logging.getLogger(__name__)
 
 
-class TestRunDevApp(pbcommand.testkit.PbTestApp):
+class TestRunDevApp(PbTestApp):
     DRIVER_BASE = "python -m pbcommand.cli.examples.dev_app "
     REQUIRES_PBCORE = True
     INPUT_FILES = [get_data_file("example.fasta")]
     TASK_OPTIONS = {"pbcommand.task_options.dev_read_length": 27}
 
 
-class TestTxtDevApp(pbcommand.testkit.PbTestApp):
+class TestTxtDevApp(PbTestApp):
     DRIVER_BASE = "python -m pbcommand.cli.examples.dev_txt_app "
     # XXX using default args, so the emit/resolve drivers are automatic
     REQUIRES_PBCORE = False
@@ -22,7 +22,7 @@ class TestTxtDevApp(pbcommand.testkit.PbTestApp):
     RESOLVED_TASK_OPTIONS = {"pbcommand.task_options.dev_max_nlines": 27}
 
 
-class TestQuickDevHelloWorld(pbcommand.testkit.PbTestApp):
+class TestQuickDevHelloWorld(PbTestApp):
     """Runs dev_qhello_world """
     DRIVER_EMIT = "python -m pbcommand.cli.examples.dev_quick_hello_world  emit-tool-contract pbcommand.tasks.dev_qhello_world "
     DRIVER_RESOLVE = "python -m pbcommand.cli.examples.dev_quick_hello_world  run-rtc "
@@ -33,7 +33,7 @@ class TestQuickDevHelloWorld(pbcommand.testkit.PbTestApp):
     RESOLVED_IS_DISTRIBUTED = True
 
 
-class TestQuickTxt(pbcommand.testkit.PbTestApp):
+class TestQuickTxt(PbTestApp):
     """Runs dev_qhello_world """
     DRIVER_EMIT = "python -m pbcommand.cli.examples.dev_quick_hello_world  emit-tool-contract pbcommand.tasks.dev_txt_hello "
     DRIVER_RESOLVE = "python -m pbcommand.cli.examples.dev_quick_hello_world  run-rtc "
@@ -44,7 +44,7 @@ class TestQuickTxt(pbcommand.testkit.PbTestApp):
     RESOLVED_IS_DISTRIBUTED = False # XXX is_distributed=False in task TC!
 
 
-class TestQuickCustomTxtCustomOuts(pbcommand.testkit.PbTestApp):
+class TestQuickCustomTxtCustomOuts(PbTestApp):
     """Runs dev_qhello_world """
     DRIVER_EMIT = "python -m pbcommand.cli.examples.dev_quick_hello_world  emit-tool-contract pbcommand.tasks.dev_txt_custom_outs "
     DRIVER_RESOLVE = "python -m pbcommand.cli.examples.dev_quick_hello_world  run-rtc "
@@ -53,7 +53,7 @@ class TestQuickCustomTxtCustomOuts(pbcommand.testkit.PbTestApp):
     INPUT_FILES = [get_data_file("example.txt")]
 
 
-class TestOptionTypes(pbcommand.testkit.PbTestApp):
+class TestOptionTypes(PbTestApp):
     DRIVER_BASE = "python -m pbcommand.cli.examples.dev_mixed_app"
     REQUIRES_PBCORE = False
     INPUT_FILES = [get_data_file("example.txt")]
