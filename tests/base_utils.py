7a5856775805868e5e2ce5c35adb53e82dc50c0c
mpkocher <michael.kocher@me.com>
2017-01-08 19:09:22 -0800

    Add TC v1 tests (should be removed when we drop TC v1 support)

--- tests/base_utils.py
+++ tests/base_utils.py
@@ -6,6 +6,7 @@ from pbcommand.testkit.base_utils import *
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
 
 DATA_DIR_TC = os.path.join(DATA_DIR, 'tool-contracts')
+DATA_DIR_TC_V1 = os.path.join(DATA_DIR, 'tool-contracts-v1')
 DATA_DIR_RTC = os.path.join(DATA_DIR, 'resolved-tool-contracts')
 DATA_DIR_PRESETS = os.path.join(DATA_DIR, "pipeline-presets")
 DATA_DIR_DSVIEW = os.path.join(DATA_DIR, "pipeline-datastore-view-rules")
@@ -22,6 +23,10 @@ def get_data_file_from_subdir(subdir, path):
 
 def get_tool_contract(name):
     return os.path.join(DATA_DIR_TC, name)
+
+
+def get_tool_contract_v1(name):
+    return os.path.join(DATA_DIR_TC_V1, name)
 
 
 def get_resolved_tool_contract(name):
