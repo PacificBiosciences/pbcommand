4b9da9c571a7ad1e4149d4b5382a344062f5bed3
mpkocher <michael.kocher@me.com>
2017-01-27 09:04:48 -0800

    Fix cyclic-dependencies ordering issue.

--- pbcommand/services/__init__.py
+++ pbcommand/services/__init__.py
@@ -1,4 +1,6 @@
-from .service_access_layer import ServiceAccessLayer
 from .models import (JobExeError, JobResult, LogLevels,
                      ServiceResourceTypes, JobTypes, JobStates,
                      ServiceJob, ServiceEntryPoint)
+# this module imports the models, so the model loading
+# must be called first to avoid cyclic-dependencies
+from .service_access_layer import ServiceAccessLayer
