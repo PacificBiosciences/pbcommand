22054cbf231b153961e0c821f85ea5a42f54d537
natechols <nathaniel.echols@gmail.com>
2019-07-24 14:52:37 -0700

    adjust behavior to be more testing-friendly

--- pbcommand/services/_service_access_layer.py
+++ pbcommand/services/_service_access_layer.py
@@ -356,9 +356,10 @@ def _to_relative_tasks_url(job_type):
 
 
 def _show_deprecation_warning(msg):
-    warnings.simplefilter('once', DeprecationWarning)
-    warnings.warn(msg, DeprecationWarning)
-    warnings.simplefilter('default', DeprecationWarning)  # reset filte
+    if not "PB_TEST_MODE" in os.environ:
+        warnings.simplefilter('once', DeprecationWarning)
+        warnings.warn(msg, DeprecationWarning)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filte
 
 
 class ServiceAccessLayer(object):  # pragma: no cover
@@ -1344,7 +1345,7 @@ def get_smrtlink_client(host, port, user=None, password=None, sleep_time=5):  # 
     the appropriate client class given the input parameters.  Unlike the client
     itself this hardcodes 8243 as the WSO2 port number.
     """
-    if host != "localhost":
+    if host != "localhost" or not None in [user, password]:
         return SmrtLinkAuthClient(host, user, password, sleep_time=sleep_time)
     else:
         return ServiceAccessLayer(host, port, sleep_time=sleep_time)
