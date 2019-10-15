930b5e54f45b004e2ab83127967c7aa318f1abfb
natechols <nathaniel.echols@gmail.com>
2019-10-15 14:54:47 -0700

    [SL-4872] add new 'file' option type for SL

--- pbcommand/models/common.py
+++ pbcommand/models/common.py
@@ -84,16 +84,12 @@ class TaskTypes(object):
 
 class TaskOptionTypes(object):
     """Core Task Option type id type"""
-    # FIXME(mpkocher)(2016-7-16) This should be more well defined, e.g., int32 and use the same id format of
-    # For example, pacbio.option_types.int32
 
-    # Because of the Avro schema restrictions and to keep the keys short
-    # in name, we'll use a dot let format. The legacy format used
-    # pbsmrtpipe.option_types.* as the root namespace
     INT = "integer"
     BOOL = "boolean"
     STR = "string"
     FLOAT = "float"
+    FILE = "file"
     # Choice type Options
     CHOICE_STR = "choice_string"
     CHOICE_INT = "choice_integer"
@@ -103,7 +99,7 @@ class TaskOptionTypes(object):
     def ALL(cls):
         """Return a set of all Task Option Types"""
         return {cls.INT, cls.BOOL, cls.STR, cls.FLOAT, cls.CHOICE_STR,
-                cls.CHOICE_INT, cls.CHOICE_FLOAT}
+                cls.CHOICE_INT, cls.CHOICE_FLOAT, cls.FILE}
 
     @classmethod
     def _raise_value_error(cls, value, allowed, option_type_name):
@@ -115,7 +111,7 @@ class TaskOptionTypes(object):
     @classmethod
     def ALL_SIMPLE(cls):
         """Returns a set of 'simple' task option types (e.g., boolean, string, int, float)"""
-        return {cls.STR, cls.BOOL, cls.INT, cls.FLOAT}
+        return {cls.STR, cls.BOOL, cls.INT, cls.FLOAT, cls.FILE}
 
     @classmethod
     def from_simple_str(cls, sx):
@@ -168,6 +164,8 @@ class TaskOptionTypes(object):
             return cls.INT
         elif isinstance(val, float):
             return cls.FLOAT
+        elif isinstance(val, None):  # XXX special case
+            return cls.FILE
         return cls.STR
 
 
