5fff35cfe24692a21707ecd226a753f1c399496e
Nathaniel Echols <nechols@pacificbiosciences.com>
2020-10-19 15:10:15 -0700

    [SL-6398] alternative client invocation for CLI tools

--- pbcommand/services/__init__.py
+++ pbcommand/services/__init__.py
@@ -1,10 +1,12 @@
 from .models import (JobExeError, JobResult, LogLevels,
                      ServiceResourceTypes, JobTypes, JobStates,
-                     ServiceJob, ServiceEntryPoint)
+                     ServiceJob, ServiceEntryPoint,
+                     add_smrtlink_server_args)
 # this module imports the models, so the model loading
 # must be called first to avoid cyclic-dependencies
 from ._service_access_layer import (ServiceAccessLayer,
                                     SmrtLinkAuthClient,
-                                    get_smrtlink_client)
+                                    get_smrtlink_client,
+                                    get_smrtlink_client_from_args)
 # There's some crufty legacy naming. Using a cleaner model here
 SmrtLinkClient = ServiceAccessLayer
